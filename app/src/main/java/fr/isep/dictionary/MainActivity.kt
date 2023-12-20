c1790c69152a0621bde7d210c5f1e805039d7ce1
L <globe_909@hotmail.com>
2023-12-20 10:03:50 +0100

    trying to see if it works

--- app/src/main/java/fr/isep/dictionary/MainActivity.kt
+++ app/src/main/java/fr/isep/dictionary/MainActivity.kt
@@ -2,7 +2,7 @@ package fr.isep.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//hei! testing github
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
